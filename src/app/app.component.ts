33e2c397e987235f0efa1c454383dc3bb4a0d782
Dennis <dennis.boanini01@gmail.com>
2018-05-01 15:01:20 +0200

    Add package for cookie consent law

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { AuthService, SocialUser } from './auth.service';
 import { Observable } from 'rxjs/Observable';
 import { MatIconRegistry } from '@angular/material/icon';
+import { Subscription } from 'rxjs/Subscription';
+import { NgcCookieConsentService, NgcInitializeEvent, NgcStatusChangeEvent } from 'ngx-cookieconsent';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'myBudget';
-  isLoggedIn$: Observable<boolean>;
+export class AppComponent implements OnInit, OnDestroy {
+	private popupOpenSubscription: Subscription;
+	private popupCloseSubscription: Subscription;
+	private initializeSubscription: Subscription;
+	private statusChangeSubscription: Subscription;
+	private revokeChoiceSubscription: Subscription;
 
-  constructor(
-    private authService: AuthService,
-    private domSanitizer: DomSanitizer,
-    private matIconRegistry: MatIconRegistry,
-  ) {
-    this.matIconRegistry.addSvgIcon(
-      `facebook_icon`,
-      domSanitizer.bypassSecurityTrustResourceUrl('../assets/fb.svg'),
-    );
-    this.matIconRegistry.addSvgIcon(
-      `google_icon`,
-      domSanitizer.bypassSecurityTrustResourceUrl('../assets/google.svg'),
-    );
-  }
+	title = 'myBudget';
+	isLoggedIn$: Observable<boolean>;
 
-  ngOnInit(): void {
-    this.isLoggedIn$ = this.authService.isLoggedIn;
-  }
+	constructor(
+		private authService: AuthService,
+		private domSanitizer: DomSanitizer,
+		private matIconRegistry: MatIconRegistry,
+		private ccService: NgcCookieConsentService
+	) {
+		this.matIconRegistry.addSvgIcon(
+			`facebook_icon`,
+			domSanitizer.bypassSecurityTrustResourceUrl('../assets/fb.svg')
+		);
+		this.matIconRegistry.addSvgIcon(
+			`google_icon`,
+			domSanitizer.bypassSecurityTrustResourceUrl('../assets/google.svg')
+		);
+	}
 
-  logOut(): void {
-    this.authService.signOut();
-  }
+	ngOnInit(): void {
+		this.isLoggedIn$ = this.authService.isLoggedIn;
+
+		// subscribe to cookieconsent observables to react to main events
+		this.popupOpenSubscription = this.ccService.popupOpen$.subscribe(() => {
+			// you can use this.ccService.getConfig() to do stuff...
+		});
+
+		this.popupCloseSubscription = this.ccService.popupClose$.subscribe(
+			() => {
+				// you can use this.ccService.getConfig() to do stuff...
+			}
+		);
+
+		this.initializeSubscription = this.ccService.initialize$.subscribe(
+			(event: NgcInitializeEvent) => {
+				// you can use this.ccService.getConfig() to do stuff...
+			}
+		);
+
+		this.statusChangeSubscription = this.ccService.statusChange$.subscribe(
+			(event: NgcStatusChangeEvent) => {
+				// you can use this.ccService.getConfig() to do stuff...
+			}
+		);
+
+		this.revokeChoiceSubscription = this.ccService.revokeChoice$.subscribe(
+			() => {
+				// you can use this.ccService.getConfig() to do stuff...
+			}
+		);
+	}
+
+	ngOnDestroy() {
+		// unsubscribe to cookieconsent observables to prevent memory leaks
+		this.popupOpenSubscription.unsubscribe();
+		this.popupCloseSubscription.unsubscribe();
+		this.initializeSubscription.unsubscribe();
+		this.statusChangeSubscription.unsubscribe();
+		this.revokeChoiceSubscription.unsubscribe();
+	}
+
+	logOut(): void {
+		this.authService.signOut();
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { SocialLoginModule, AuthServiceConfig } from 'angularx-social-login';
 import { environment } from '../environments/environment';
+import {
+	NgcCookieConsentModule,
+	NgcCookieConsentConfig
+} from 'ngx-cookieconsent';
 
 import { AppRoutingModule } from './app.routing';
 
 import {
-  MatToolbarModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatButtonModule,
-  MatCardModule,
-  MatIconModule,
-  MatProgressSpinnerModule,
+	MatToolbarModule,
+	MatFormFieldModule,
+	MatInputModule,
+	MatButtonModule,
+	MatCardModule,
+	MatIconModule,
+	MatProgressSpinnerModule
 } from '@angular/material';
 
 import { AuthGuard } from './auth.guard';
@@ -28,34 +32,64 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 const provideConfig = () => environment.socialConfig;
 
+const cookieConfig: NgcCookieConsentConfig = {
+	cookie: {
+		domain: 'http://www.mubudget.tk/'
+	},
+	position: 'bottom-left',
+	theme: 'classic',
+	palette: {
+		popup: {
+			background: '#000000',
+			text: '#ffffff',
+			link: '#ffffff'
+		},
+		button: {
+			background: '#f1d600',
+			text: '#000000',
+			border: 'transparent'
+		}
+	},
+	type: 'info',
+	content: {
+		message:
+			'This website uses cookies to ensure you get the best experience on our website.',
+		dismiss: 'Got it!',
+		deny: 'Refuse cookies',
+		link: 'Learn more',
+		href: 'https://cookiesandyou.com'
+	}
+};
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    DashboardComponent,
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    FlexLayoutModule,
-    SocialLoginModule,
-    MatToolbarModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCardModule,
-    MatIconModule,
-    MatProgressSpinnerModule,
-  ],
-  providers: [
-    { provide: AuthServiceConfig, useFactory: provideConfig },
-    AuthService,
-    AuthGuard,
-  ],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		LoginComponent,
+		RegisterComponent,
+		DashboardComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		HttpClientModule,
+		AppRoutingModule,
+		ReactiveFormsModule,
+		FlexLayoutModule,
+		SocialLoginModule,
+		MatToolbarModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatButtonModule,
+		MatCardModule,
+		MatIconModule,
+		MatProgressSpinnerModule,
+		NgcCookieConsentModule.forRoot(cookieConfig)
+	],
+	providers: [
+		{ provide: AuthServiceConfig, useFactory: provideConfig },
+		AuthService,
+		AuthGuard
+	],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
